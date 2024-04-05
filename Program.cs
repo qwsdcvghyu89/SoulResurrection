67191d34520385924e16446d0a5d86f96069e7de
aeqw <qwsdcvghyu89@gmail.com>
2024-04-05 20:55:46 +0300

    Added correct Main method for Synthesis. Added dependency check.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
-﻿namespace SoulResurrection {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+﻿using Mutagen.Bethesda;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
+
+namespace SoulResurrection {
+    public class Program {
+        public static async Task<int> Main(string[] args) {
+            if (!OperatingSystem.IsWindowsVersionAtLeast(7))
+                return -10;
+            return await SynthesisPipeline.Instance
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .AddRunnabilityCheck(IsMainModPresent)
+                .SetTypicalOpen(GameRelease.SkyrimSE, "SoulResurrectionPatch.esp")
+                .Run(args);
+        }
+
+        private static void IsMainModPresent(IRunnabilityState state) {
+            if (!state.RawLoadOrder.Any((x) => x.FileName == "SoulResurrection.esp"))
+                throw new Exception("Missing master!");
+        }
+
+        private static async Task RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state) {
+            // Get NPCs
+            // Filter
+            // Add MarrowItemCopy
         }
     }
 }
